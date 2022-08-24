cdaf64dd3454c5dec782b999030ec852d2acb32a
Jackson Oppenheim <50721527+jacksonopp@users.noreply.github.com>
2022-08-24 18:20:57 -0500

    oopsie

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -1,12 +1,16 @@
 // src/server/router/context.ts
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
+import {
+  Session,
+  unstable_getServerSession as getServerSession,
+} from "next-auth";
+import { authOptions as nextAuthOptions } from "../../pages/api/auth/[...nextauth]";
 import { prisma } from "../db/client";
 
-/**
- * Replace this with an object if you want to pass things to createContextInner
- */
-type CreateContextOptions = Record<string, never>;
+type CreateContextOptions = {
+  session: Session | null;
+};
 
 /** Use this helper for:
  * - testing, where we dont have to Mock Next.js' req/res
@@ -14,6 +18,7 @@ type CreateContextOptions = Record<string, never>;
  **/
 export const createContextInner = async (opts: CreateContextOptions) => {
   return {
+    session: opts.session,
     prisma,
   };
 };
@@ -25,7 +30,11 @@ export const createContextInner = async (opts: CreateContextOptions) => {
 export const createContext = async (
   opts: trpcNext.CreateNextContextOptions,
 ) => {
-  return await createContextInner({});
+  const session = await getServerSession(opts.req, opts.res, nextAuthOptions);
+
+  return await createContextInner({
+    session,
+  });
 };
 
 type Context = trpc.inferAsyncReturnType<typeof createContext>;
